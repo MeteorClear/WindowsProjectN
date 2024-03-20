e890902dee7d0f90fd723ac7cad1e09b9e55cd47
MeteorClear <tivics9973@gmail.com>
2024-03-20 20:02:15 +0900

    초기 커밋.

---
+++ MainPage.xaml.cpp
@@ -0,0 +1,67 @@
+﻿//
+// MainPage.xaml.cpp
+// MainPage 클래스의 구현입니다.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace ProjectN;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// 빈 페이지 항목 템플릿에 대한 설명은 https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x412에 나와 있습니다.
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+}
+
+
+
+// 폴더 경로를 입력받아 표시
+
+using namespace Windows::Storage;
+using namespace Windows::Storage::Pickers;
+
+void MainPage::SelectFolder_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+    auto picker = ref new FolderPicker();
+    picker->SuggestedStartLocation = PickerLocationId::Desktop;
+    picker->FileTypeFilter->Append("*");
+    picker->ViewMode = PickerViewMode::List;
+
+    // Show the folder picker
+    concurrency::create_task(picker->PickSingleFolderAsync()).then([this](StorageFolder^ folder)
+    {
+        if (folder != nullptr)
+        {
+            // Folder selected, display the path
+            this->FolderPathTextBox->Text = folder->Path;
+            this->SelectedFolderPathTextBlock->Text = "Selected Folder: " + folder->Path;
+        }
+        else
+        {
+            // Operation cancelled
+            this->SelectedFolderPathTextBlock->Text = "No folder selected.";
+        }
+    });
+}
+
+
+
+// 폴더 경로의 파일들의 파일명 표시
+
+#include <memory>
+
+using namespace Windows::UI::Core;
+
---
+++ MainPage.xaml.h
@@ -0,0 +1,27 @@
+﻿//
+// MainPage.xaml.h
+// MainPage 클래스의 선언입니다.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+using namespace Windows::UI::Core;
+
+namespace ProjectN
+{
+	/// <summary>
+	/// 자체적으로 사용하거나 프레임 내에서 탐색할 수 있는 빈 페이지입니다.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	private:
+		
+	public:
+		MainPage();
+
+
+		void SelectFolder_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+
+	};
+}
